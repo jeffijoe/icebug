132ba0b51d94bf7dfcbb714c63ac3bc4bbb24689
Jeff Hansen <jeffijoe@hotmail.com>
2016-02-27 01:50:00 +0100

    Removed use of spread-operator, should fix issue #3.

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 console.log('Example started.');
 setInterval(() => {
-  const content = fs.readFileSync('example.txt');
+  const content = fs.readFileSync(__dirname + '/example.txt');
   const asString = content.toString();
   console.log('-----', new Date(), '-----');
   console.log(asString);
